83a1aaa17358bdc24a081f6a006bc1c64f443149
lizhengliang <1731415266@qq.com>
2022-08-01 11:29:42 +0800

    leetcode 90. 子集 II

--- src/main/java/com/lzl/App.java
+++ src/main/java/com/lzl/App.java
@@ -89,6 +89,79 @@ public class App {
     }
 
     /**
+     * 60. 排列序列
+     * 给出集合 [1,2,3,...,n]，其所有元素共有 n! 种排列。
+     *
+     * 按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下：
+     *
+     * "123"
+     * "132"
+     * "213"
+     * "231"
+     * "312"
+     * "321"
+     * 给定 n 和 k，返回第 k 个排列。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：n = 3, k = 3
+     * 输出："213"
+     * 示例 2：
+     *
+     * 输入：n = 4, k = 9
+     * 输出："2314"
+     * 示例 3：
+     *
+     * 输入：n = 3, k = 1
+     * 输出："123"
+     * @param n
+     * @param k
+     * @return
+     */
+    public String getPermutation(int n, int k) {
+
+    }
+    /**
+     * 90. 子集 II
+     * 给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+     *
+     * 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [1,2,2]
+     * 输出：[[],[1],[1,2],[1,2,2],[2],[2,2]]
+     * 示例 2：
+     *
+     * 输入：nums = [0]
+     * 输出：[[],[0]]
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        Deque<Integer> path=new ArrayDeque<>();
+        final int l = nums.length;
+        Arrays.sort(nums);
+        dfs5(path, nums, res,l,0);
+        return res;
+    }
+
+    private void dfs5(Deque<Integer> path, int[] nums, List<List<Integer>> res, int l, int begin) {
+        res.add(new ArrayList<>(path));
+        for (int i = begin; i < l; i++) {
+            if(i>begin&&nums[i]==nums[i-1])continue;
+            path.addLast(nums[i]);
+            dfs5(path, nums, res, l, i+1);
+            path.removeLast();
+        }
+    }
+
+    /**
      * 78. 子集
      * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
      * <p>
