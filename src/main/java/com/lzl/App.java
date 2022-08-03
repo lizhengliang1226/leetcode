a1a1b73ab4ce48fb2b2c141c8f371597cd10da0b
lizhengliang <1731415266@qq.com>
2022-08-03 10:19:05 +0800

    leetcode 733. 图像渲染

--- src/main/java/com/lzl/App.java
+++ src/main/java/com/lzl/App.java
@@ -13,7 +13,7 @@ import java.util.*;
 
 public class App {
 
-    private ModuleClass app;
+    private final static App app = new App();
 
     /**
      * 35. 搜索插入位置
@@ -59,34 +59,66 @@ public class App {
     }
 
     private void print(String... strs) {
-        System.out.println( Arrays.toString(strs));
+        System.out.println(Arrays.toString(strs));
     }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        final App app = new App();
-//        System.out.println(app.myAtoi("+-3"));
-//    app.isPalindrome(1233321);
-//        System.out.println(app.intToRoman(3356));
-//        app.romanToInt("MMMCCCLVI");
-//        System.out.println(app.longestCommonPrefix(new String[]{"abcdf", "abc", "ab"}));
-//        app.threeSum(new int[]{2, 3, -5, 8, 9, -17, 3, 5, 6, 71, 2, 3, -4, -9, -2, -78});
-//        System.out.println(app.threeSumClosest(new int[]{-3, -2, -5,3, -4}, -1));
-//        System.out.println(app.letterCombinations("233"));
-//        System.out.println(app.fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0));
-//        System.out.println(app.isValid("()"));
-//        System.out.println(app.generateParenthesis(6));
-//        app.mergeKLists(new ListNode[]{LeetCodeUtil.getSortList(1), null, LeetCodeUtil.getSortList(1)});
-//        final ListNode listNode = app.swapPairs(LeetCodeUtil.getSortList(7));
-//        traverse(listNode);
-//        final ListNode listNode = app. reverseKGroup(LeetCodeUtil.getSortList(2), 2);
-//        traverse(listNode);
-//        System.out.println(app.removeDuplicates(new int[]{1, 1, 2, 2, 3, 3}));
-//        System.out.println(app.strStr("abababaaabaabababaabddd", "aabaabababaab"));
-//        System.out.println(app.divide(16,-3));
-//    app.nextPermutation(LeetCodeUtil.getIntAry(6,1,LeetCodeUtil.NO_SORT));
-//        LeetCodeUtil.traverseAry(app.searchRange(new int[]{1}, 1));
         System.out.println(app.restoreIpAddresses("101023"));
+    }
+
+    /**
+     * 733. 图像渲染
+     * 有一幅以 m x n 的二维整数数组表示的图画 image ，其中 image[i][j] 表示该图画的像素值大小。
+     * <p>
+     * 你也被给予三个整数 sr ,  sc 和 newColor 。你应该从像素 image[sr][sc] 开始对图像进行 上色填充 。
+     * <p>
+     * 为了完成 上色工作 ，从初始像素开始，记录初始坐标的 上下左右四个方向上 像素值与初始坐标相同的相连像素点
+     * ，接着再记录这四个方向上符合条件的像素点与他们对应 四个方向上 像素值与初始坐标相同的相连像素点，……，重复该过程
+     * 。将所有有记录的像素点的颜色值改为 newColor 。
+     * <p>
+     * 最后返回 经过上色渲染后的图像 。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1:
+     * <p>
+     * <p>
+     * <p>
+     * 输入: image = [[1,1,1],[1,1,0],[1,0,1]]，sr = 1, sc = 1, newColor = 2
+     * 输出: [[2,2,2],[2,2,0],[2,0,1]]
+     * 解析: 在图像的正中间，(坐标(sr,sc)=(1,1)),在路径上所有符合条件的像素点的颜色都被更改成2。
+     * 注意，右下角的像素没有更改为2，因为它不是在上下左右四个方向上与初始点相连的像素点。
+     * 示例 2:
+     * <p>
+     * 输入: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, newColor = 2
+     * 输出: [[2,2,2],[2,2,2]]
+     *
+     * @param image
+     * @param sr
+     * @param sc
+     * @param color
+     * @return
+     */
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int col=image[sr][sc];
+        if(col!=color){
+            dfs8(image,sr,sc,col,color);
+        }
+        return image;
+    }
+    private int[] x=new int[]{0,0,1,-1};
+    private int[] y=new int[]{-1,1,0,0};
+    private void dfs8(int[][] image, int sr, int sc, int oldColor, int newColor) {
+        if(image[sr][sc]==oldColor){
+            image[sr][sc] = newColor;
+            for (int i = 0; i < 4; i++) {
+                int xx=sr+x[i];
+                int yy=sc+y[i];
+                if(xx>=0&&xx<image.length&&yy>=0&&yy<image[0].length)
+                dfs8(image,xx,yy,oldColor,newColor);
+            }
+        }
     }
 
     /**
@@ -125,9 +157,9 @@ public class App {
     }
 
     private void dfs7(int[] path, List<String> res, String s, int segId, int begin) {
-        print("ip段数："+segId+"段","搜素字符起始位置："+begin,"此时ip值："+Arrays.toString(path));
+        print("ip段数：" + segId + "段", "搜素字符起始位置：" + begin, "此时ip值：" + Arrays.toString(path));
         if (segId == 4) {
-            print("已找满四段ip，此段ip为：",Arrays.toString(path));
+            print("已找满四段ip，此段ip为：", Arrays.toString(path));
             if (begin == s.length()) {
                 StringBuilder sb = new StringBuilder();
                 for (int i = 0; i < 4; i++) {
@@ -136,7 +168,7 @@ public class App {
                         sb.append(".");
                     }
                 }
-                print("找到结果集",Arrays.toString(path));
+                print("找到结果集", Arrays.toString(path));
                 res.add(sb.toString());
             }
             return;
@@ -147,10 +179,10 @@ public class App {
         if (s.charAt(begin) == '0') {
             print("该位置为0，直接作为此段ip值，不再搜索");
             path[segId] = 0;
-            print("此时ip值："+Arrays.toString(path));
-            print("dfs7压栈","取字符位置："+(begin+1),"ip段："+(segId+1));
+            print("此时ip值：" + Arrays.toString(path));
+            print("dfs7压栈", "取字符位置：" + (begin + 1), "ip段：" + (segId + 1));
             dfs7(path, res, s, segId + 1, begin + 1);
-            print("dfs7出栈","回溯字符位置："+begin,"ip段："+segId+"段");
+            print("dfs7出栈", "回溯字符位置：" + begin, "ip段：" + segId + "段");
 
         }
         // 某一段的ip地址的暂存值
@@ -160,10 +192,10 @@ public class App {
             print("取到地址addr：", String.valueOf(addr));
             if (addr > 0 && addr <= 0xff) {
                 path[segId] = addr;
-                print("当前ip值：",Arrays.toString(path));
-                print("dfs7压栈","取字符位置："+i+"","ip段："+segId+"");
+                print("当前ip值：", Arrays.toString(path));
+                print("dfs7压栈", "取字符位置：" + i + "", "ip段：" + segId + "");
                 dfs7(path, res, s, segId + 1, i + 1);
-                print("dfs7出栈","回溯字符位置："+i,"下次取值位置："+(i+1),"ip段："+segId+"段");
+                print("dfs7出栈", "回溯字符位置：" + i, "下次取值位置：" + (i + 1), "ip段：" + segId + "段");
             } else {
                 break;
             }
