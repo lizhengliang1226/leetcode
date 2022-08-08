ca9c44b8b2f6f156264183fc0b78c2eedcc1c640
lizhengliang <1731415266@qq.com>
2022-08-08 17:32:27 +0800

    leetcode 37. 解数独

---
+++ src/main/java/com/lzl/util/Stack.java
@@ -0,0 +1,32 @@
+package com.lzl.util;
+
+import java.util.LinkedList;
+
+/**
+ * @author LZL
+ * @version v1.0
+ * @date 2022/8/8-13:58
+ */
+public class Stack<T> {
+    private final LinkedList<T> storage = new LinkedList<T>();
+
+    public void push(T v) {
+        storage.addFirst(v);
+    }
+
+    public T peek() {
+        return storage.getFirst();
+    }
+
+    public T pop() {
+        return storage.removeFirst();
+    }
+
+    public boolean empty() {
+        return storage.isEmpty();
+    }
+
+    public String toString() {
+        return storage.toString();
+    }
+}
