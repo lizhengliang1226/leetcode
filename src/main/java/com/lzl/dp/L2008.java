4c9ab92c283e07773d7667859858b452b520da37
lizhengliang1226 <1731415266@qq.com>
2023-12-08 11:17:43 +0800

    题目：2008. 出租车的最大盈利 解法：动态规划，以rides分组后枚举所有在i处下车能赚到的最多钱 dp[i]=dp[i-1]或者dp[i]=Math.max(dp[i],dp[前面的]+当前赚的钱)

--- src/main/java/com/lzl/dp/L2008.java
+++ src/main/java/com/lzl/dp/L2008.java
@@ -10,14 +10,15 @@ import java.util.List;
  * 相关企业
  * 提示
  * 你驾驶出租车行驶在一条有 n 个地点的路上。这 n 个地点从近到远编号为 1 到 n ，你想要从 1 开到 n ，通过接乘客订单盈利。你只能沿着编号递增的方向前进，不能改变方向。
- *
+ * <p>
  * 乘客信息用一个下标从 0 开始的二维数组 rides 表示，其中 rides[i] = [starti, endi, tipi] 表示第 i 位乘客需要从地点 starti 前往 endi ，愿意支付 tipi 元的小费。
- *
+ * <p>
  * 每一位 你选择接单的乘客 i ，你可以 盈利 endi - starti + tipi 元。你同时 最多 只能接一个订单。
- *
+ * <p>
  * 给你 n 和 rides ，请你返回在最优接单方案下，你能盈利 最多 多少元。
- *
+ * <p>
  * 注意：你可以在一个地点放下一位乘客，并在同一个地点接上另一位乘客。
+ *
  * @author lzl
  * @version 1.0
  * @since 2023/12/08
@@ -40,35 +41,36 @@ public class L2008 {
      * v0是按end分组后，value中保存的起始点v0，以及从起始到这个end赚的钱v1，
      * dp[v0]代表去找以当前这个起始点为结束点的前面的路程能赚的最多钱，再加上当前在这搭车能赚的钱v0就得到了第二种情况赚的钱
      * 取两种情况的最大值即可
+     *
      * @param n
      * @param rides
      * @return
      */
     public long maxTaxiEarnings(int n, int[][] rides) {
-        List<int[]>[] grid=new List[n+1];
+        List<int[]>[] grid = new List[n + 1];
         for (int i = 0; i < rides.length; i++) {
             int start = rides[i][0];
             int end = rides[i][1];
             int tip = rides[i][2];
-            if(grid[end]==null){
-                grid[end]=new ArrayList<>();
+            if (grid[end] == null) {
+                grid[end] = new ArrayList<>();
             }
             // 先按end分组得到在end下车能赚钱的所有旅行
-            grid[end].add(new int[]{start,end-start+tip});
+            grid[end].add(new int[]{start, end - start + tip});
         }
         // 初始化dp，默认为0
-        long[] dp=new long[n+1];
+        long[] dp = new long[n + 1];
         // 从2-n遍历，得到以i为终点能赚的最多钱
         for (int i = 2; i <= n; i++) {
             // 默认当前这个点不上人，或者没有人
-            dp[i]=dp[i-1];
+            dp[i] = dp[i - 1];
             // 说明在这个地方有人可以下车
-            if(grid[i]!=null){
+            if (grid[i] != null) {
                 // 那就遍历可以在这下车的人，取最大的那个
                 for (int[] ints : grid[i]) {
                     // dp[ints[0]]代表以当前这个下车点的那个开始点为下车点的前面的最大盈利
                     // ints[1]是当前这个人的盈利
-                    dp[i]=Math.max(dp[i],dp[ints[0]]+ints[1]);
+                    dp[i] = Math.max(dp[i], dp[ints[0]] + ints[1]);
                 }
             }
         }
