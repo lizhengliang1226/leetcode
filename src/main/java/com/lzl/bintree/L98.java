78ec6c0b01ba06dc5fe0dec9c655cefbaf7e323d
lizhengliang <1731415266@qq.com>
2024-12-22 16:47:06 +0800

    feat(bintree): 添加累加树功能并优化二叉树验证逻辑
    - 新增 L538 类实现把二叉搜索树转换为累加树的功能
    -优化 L98 类中的 isValidBST 方法，提高二叉树验证效率

--- src/main/java/com/lzl/bintree/L98.java
+++ src/main/java/com/lzl/bintree/L98.java
@@ -22,24 +22,24 @@ import com.lzl.util.TreeNode;
 public class L98 {
     public static void main(String[] args) {
         //546nn37
-        TreeNode t5=new TreeNode(5);
-        TreeNode t4=new TreeNode(4);
-        TreeNode t6=new TreeNode(6);
-        TreeNode t3=new TreeNode(3);
-        TreeNode t7=new TreeNode(7);
-        t5.left=t4;
-        t5.right=t6;
-        t6.left=t3;
-        t6.right=t7;
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t6 = new TreeNode(6);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t7 = new TreeNode(7);
+        t5.left = t4;
+        t5.right = t6;
+        t6.left = t3;
+        t6.right = t7;
         System.out.println(new L98().isValidBST(t5));
     }
+
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
     private boolean isValidBST(TreeNode root, long left, long right) {
-        if(root==null)return true;
-        int x=root.val;
-        return x>left&&x<right&&isValidBST(root.left,left,x)&&isValidBST(root.right,x,right);
+        if (root == null) return true;
+        return root.val > left && root.val < right && isValidBST(root.left, left, root.val) && isValidBST(root.right, root.val, right);
     }
 }
